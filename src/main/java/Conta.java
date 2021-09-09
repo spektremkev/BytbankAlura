5e0781138722b37da6073e0aef92b8e6612f03ff
Gabriel Dos S N Martins <50385858+spektremkev@users.noreply.github.com>
2021-09-09 19:41:06 -0300

    Aula4.0

--- src/main/java/Conta.java
+++ src/main/java/Conta.java
@@ -16,7 +16,27 @@ public class Conta {
     String titular;
     
     void deposita(double valor){
-    
+        this.saldo = this.saldo + valor;
     }
 
+    public boolean saca(double valor){
+        if(this.saldo >= valor){
+            this.saldo = this.saldo - valor;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino){
+        if(this.saldo >= valor){
+            this.saldo = this.saldo - valor;
+            destino.deposita(valor);
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
+
+
--- src/main/java/TestaMetodo.java
+++ src/main/java/TestaMetodo.java
@@ -9,5 +9,31 @@
  * @author gabri
  */
 public class TestaMetodo {
-    
+    public static void main(String[] args){
+        Conta contaDoPaulo = new Conta();
+        contaDoPaulo.saldo= 500;
+
+        contaDoPaulo.deposita(100);
+        System.out.println("paulo"+contaDoPaulo.saldo);
+        contaDoPaulo.saca(50);
+        System.out.println("paulo"+contaDoPaulo.saldo);
+        contaDoPaulo.saca(150);
+        System.out.println("paulo"+contaDoPaulo.saldo);
+
+        boolean conseguiuSacar = contaDoPaulo.saca(501);
+        System.out.println(contaDoPaulo.saldo);
+        System.out.println(conseguiuSacar);
+
+        Conta contaDaMaria = new Conta();
+        contaDaMaria.deposita(100);
+
+        if(contaDaMaria.transfere(300, contaDoPaulo)){
+            System.out.println("Transferência realizada com sucesso!");
+        }else{
+            System.out.println("Transferência não realizada");
+        }
+
+        contaDaMaria.transfere(300, contaDoPaulo);
+        System.out.println(contaDaMaria.saldo);
+    }
 }

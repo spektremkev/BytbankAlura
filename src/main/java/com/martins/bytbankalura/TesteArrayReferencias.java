7659717ef267dbf01d9ed79a5e4383bce0739e2b
Gabriel Dos S N Martins <50385858+spektremkev@users.noreply.github.com>
2021-09-26 19:32:00 -0300

    aula_01

---
+++ src/main/java/com/martins/bytbankalura/TesteArrayReferencias.java
@@ -0,0 +1,18 @@
+package com.martins.bytbankalura;
+
+public class TesteArrayReferencias {
+
+    public static void main(String[] args) {
+
+        ContaCorrente[] contas = new ContaCorrente[5];
+
+        ContaCorrente cc1 = new ContaCorrente(22, 11);
+        ContaCorrente cc2 = new ContaCorrente(22, 22);
+
+        contas[0] = cc1;
+        contas[1] = cc2;
+
+        System.out.println(contas[0].getNumero());
+    }
+
+}
